7a1e65af5e64587c3a48c555489fdc0408b9dd0c
GavinDiaper <gavindiaper@gmail.com>
2026-04-19 15:43:52 +0400

    feat: add new nodes and server chunks for user identity flow
    - Introduced new immutable nodes for handling user identity, including access and refresh tokens.
    - Added corresponding compressed and Brotli versions of the new JavaScript files.
    - Implemented server-side chunks for error handling and page rendering.
    - Updated source maps for better debugging and traceability.

---
+++ apps/user-identity/build/server/chunks/3-CHhPkoNk.js
@@ -0,0 +1,9 @@
+const index = 3;
+let component_cache;
+const component = async () => component_cache ??= (await import('./_page.svelte-BZaNrcN6.js')).default;
+const imports = ["_app/immutable/nodes/3.C2WMQ4db.js","_app/immutable/chunks/BEE9BKhQ.js","_app/immutable/chunks/Cq5QDQ4r.js","_app/immutable/chunks/DOODvaq-.js","_app/immutable/chunks/DkhWXAjQ.js","_app/immutable/chunks/CBesaTOz.js","_app/immutable/chunks/Dm3k2IPD.js","_app/immutable/chunks/GBAEcOGc.js","_app/immutable/chunks/PrrssGob.js","_app/immutable/chunks/CIj54DjY.js"];
+const stylesheets = ["_app/immutable/assets/3.B5LiLgxR.css"];
+const fonts = [];
+
+export { component, fonts, imports, index, stylesheets };
+//# sourceMappingURL=3-CHhPkoNk.js.map
--- apps/user-identity/build/server/manifest.js
+++ apps/user-identity/build/server/manifest.js
@@ -10,12 +10,12 @@ return {
 	assets: new Set([]),
 	mimeTypes: {},
 	_: {
-		client: {start:"_app/immutable/entry/start.D92PTQBz.js",app:"_app/immutable/entry/app.C-ZhmIOl.js",imports:["_app/immutable/entry/start.D92PTQBz.js","_app/immutable/chunks/DGkrOvMJ.js","_app/immutable/chunks/Cq5QDQ4r.js","_app/immutable/chunks/DkhWXAjQ.js","_app/immutable/entry/app.C-ZhmIOl.js","_app/immutable/chunks/Cq5QDQ4r.js","_app/immutable/chunks/CBesaTOz.js","_app/immutable/chunks/BEE9BKhQ.js","_app/immutable/chunks/DkhWXAjQ.js","_app/immutable/chunks/GBAEcOGc.js","_app/immutable/chunks/Dm3k2IPD.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.DxgvBL9D.js",app:"_app/immutable/entry/app.DsOFnhjV.js",imports:["_app/immutable/entry/start.DxgvBL9D.js","_app/immutable/chunks/CIj54DjY.js","_app/immutable/chunks/Cq5QDQ4r.js","_app/immutable/chunks/DkhWXAjQ.js","_app/immutable/entry/app.DsOFnhjV.js","_app/immutable/chunks/Cq5QDQ4r.js","_app/immutable/chunks/CBesaTOz.js","_app/immutable/chunks/BEE9BKhQ.js","_app/immutable/chunks/DkhWXAjQ.js","_app/immutable/chunks/GBAEcOGc.js","_app/immutable/chunks/Dm3k2IPD.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./chunks/0-CSkKHpth.js')),
-			__memo(() => import('./chunks/1-DoVlGte3.js')),
-			__memo(() => import('./chunks/2-BxoRqEZ4.js')),
-			__memo(() => import('./chunks/3-VoeKOfDW.js'))
+			__memo(() => import('./chunks/1-Cj0Q3ueF.js')),
+			__memo(() => import('./chunks/2-D4L_Yy9B.js')),
+			__memo(() => import('./chunks/3-CHhPkoNk.js'))
 		],
 		remotes: {
 			
